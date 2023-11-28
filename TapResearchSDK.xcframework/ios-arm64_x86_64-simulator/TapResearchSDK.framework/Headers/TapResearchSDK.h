fb3145d132ffb5328bb2384c3abfe31761a2c013
Jeroen Verbeek <jeroenverbeek@mac.com>
2023-11-28 14:44:51 -0800

    Update the ramework to 3.2.0-beta2

---
+++ TapResearchSDK.xcframework/ios-arm64_x86_64-simulator/TapResearchSDK.framework/Headers/TapResearchSDK.h
@@ -0,0 +1,18 @@
+//
+//  TapResearchSDK.h
+//  TapResearchSDK
+//
+//  Created by Jeroen Verbeek on 2/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapResearchSDK.
+FOUNDATION_EXPORT double TapResearchSDKVersionNumber;
+
+//! Project version string for TapResearchSDK.
+FOUNDATION_EXPORT const unsigned char TapResearchSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapResearchSDK/PublicHeader.h>
+
+
